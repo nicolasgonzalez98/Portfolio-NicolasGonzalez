da32f6262acc0ed3833609b6d99f10e4beb0d227
unknown <nicolasgonzalez470@gmail.com>
2022-07-22 15:00:10 -0300

    first commit

---
+++ components/layouts/main.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+import { Box, Container } from '@chakra-ui/react'
+
+const Main = ({children, router}) => {
+    return (
+        <Box 
+            as='main' 
+            pb={8}
+            
+        >
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                {/* 
+                <meta name="description" content="Nicolas's portfolio" />
+                <meta name="author" content="Takuya Matsuyama" />
+                <meta name="author" content="craftzdog" />
+                <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+                <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@craftzdog" />
+                <meta name="twitter:creator" content="@craftzdog" />
+                <meta name="twitter:image" content="/card.png" />
+                <meta property="og:site_name" content="Takuya Matsuyama's Homepage" />
+                <meta property="og:type" content="website" />
+                <meta property="og:image" content="/card.png" />  */}
+                <title>Nicolas Gonzalez Portfolio</title>
+            </Head>
+
+            <Container >
+                {children}
+            </Container>
+        </Box>
+    )
+}
+
+export default Main;
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Home = () => {
+    return (
+        <Container >
+            <Box 
+            borderRadius="lg" 
+            color='white'
+            bg='red' 
+            p={3} 
+            mb={6} 
+            align='center'
+            >
+                Hello I&apos;m a full-stack developer based in Buenos Aires, Argentina! 
+            </Box>
+
+            <Box display={{md:'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as='h2' variant='page-title'>
+                        Nicolas Gonzalez
+                    </Heading>
+                    <p>Full Stack Developer</p>
+                </Box>
+            </Box>
+
+            
+        </Container>
+    )
+}
+
+export default Home;
