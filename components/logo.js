4f792a576c310d1a40bcdfd37c7f3554936b295a
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2022-07-23 21:26:28 -0300

    'navbar'

--- components/logo.js
+++ components/logo.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
+import img from "./images/footprint.png";
+
 
 const LogoBox = styled.span`
     font-weight: bold;
@@ -20,13 +22,13 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+    //const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
   
     return (
       <Link href="/" scroll={false}>
         <a>
           <LogoBox>
-            <Image src={footPrintImg} width={20} height={20} alt="logo" />
+            <Image src={img} width={20} height={20} alt="logo" />
             <Text
               color={useColorModeValue('gray.800', 'whiteAlpha.900')}
               fontFamily='M PLUS Rounded 1c", sans-serif'
--- components/navbar.js
+++ components/navbar.js
@@ -14,10 +14,8 @@ import {
     IconButton,
     useColorModeValue
 } from '@chakra-ui/react'
-// import { HamburgerIcon } from '@chakra-ui/icons'
-//import ThemeToggleButton from './theme-toggle-button'
-// import { IoLogoGithub } from 'react-icons/io5'
-
+import { HamburgerIcon } from '@chakra-ui/icons'
+import { IoLogoGithub } from 'react-icons/io5'
 
 const LinkItem = ({href, path, children}) => {
     const active = path === href
@@ -29,8 +27,6 @@ const LinkItem = ({href, path, children}) => {
             p={2}
             bg={active ? 'grassTeal' : undefined}
             color={active ? '#202023' : inactiveColor}
-            // target={target}
-            // {...props}
           >
             {children}
           </Link>
@@ -65,6 +61,63 @@ const Navbar = props => {
                         <Logo />
                     </Heading>
                 </Flex>
+                <Stack
+                    direction={{base: 'column', md: 'row'}}
+                    display={{base:'none', md: 'flex'}}
+                    width={{base:'full', md:'auto'}}
+                    alignItems='center'
+                    flexGrow={1}
+                    mt={{ base: 4, nmd: 0}}
+                >
+                    <LinkItem 
+                        href='/works'
+                        path={path}
+                    >
+                        Works
+                    </LinkItem>
+                    <LinkItem 
+                        href='/posts'
+                        path={path}
+                    >
+                        Posts
+                    </LinkItem>
+                    
+                </Stack>
+
+                <Box flex={1} align='right'>
+                    <Box 
+                        ml={2}
+                        display={{base:'inline-block',md:'none'}}
+                    >
+                        <Menu>
+                            <MenuButton 
+                                as={IconButton} 
+                                icon={<HamburgerIcon />} 
+                                variant='outline' 
+                                aria-label="Options" 
+                            />
+                            <MenuList>
+                                <NextLink href="/" passHref>
+                                    <MenuItem as={Link}>About</MenuItem>
+                                </NextLink>
+                                <NextLink href="/works" passHref>
+                                    <MenuItem as={Link}>Works</MenuItem>
+                                </NextLink>
+                                <NextLink href="/posts" passHref>
+                                    <MenuItem as={Link}>Posts</MenuItem>
+                                </NextLink>
+                                <MenuItem
+                                target='_blank'
+                                as={Link}
+                                href="https://github.com/nicolasgonzalez98"
+                                >
+                                    View GitHub
+                                </MenuItem>
+                            </MenuList>
+
+                        </Menu>
+                    </Box>
+                </Box>
             </Container>
         </Box>
     )
