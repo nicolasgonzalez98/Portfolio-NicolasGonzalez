cef06776317b0332cbad46538e599937f4239cd1
unknown <nicolasgonzalez470@gmail.com>
2022-07-25 00:45:39 -0300

    add profile photo

--- components/logo.js
+++ components/logo.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Text, useColorModeValue } from '@chakra-ui/react'
 import styled from '@emotion/styled'
-import img from './images/footprint.png'
 
 const LogoBox = styled.span`
     font-weight: bold;
@@ -21,13 +20,13 @@ const LogoBox = styled.span`
 `
 
 const Logo = () => {
-    //const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
+    const footPrintImg = `/images/footprint${useColorModeValue('', '-dark')}.png`
   
     return (
       <Link href="/" scroll={false}>
         <a>
           <LogoBox>
-            <Image src={img} width={20} height={20} alt="logo" />
+            <Image src={footPrintImg} width={20} height={20} alt="logo" />
             <Text
               color={useColorModeValue('gray.800', 'whiteAlpha.900')}
               fontFamily='M PLUS Rounded 1c", sans-serif'
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+
 
 const Home = () => {
     return (
         <Container >
             <Box 
             borderRadius="lg"
-            bg='red' 
+            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
             p={3} 
             mb={6} 
             align='center'
@@ -20,7 +21,26 @@ const Home = () => {
                     </Heading>
                     <p>Full Stack Developer</p>
                 </Box>
+                <Box 
+                    flexShrink={0}
+                    mt={{base:4,md:0}}
+                    ml={{md:6}}
+                    align='center'
+
+                >
+                    <Image 
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle='solid'
+                        maxWidth='100px'
+                        display='inline-block'
+                        borderRadius="full"
+                        src='/images/nico_edit.jpg'
+                        alt='Profile Image'
+                    />
+                </Box>
             </Box>
+
 
             
         </Container>
