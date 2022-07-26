1ca442469c0e4bce7f4e63884f86d8cd64dedfe1
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2022-07-26 04:09:58 -0300

    'page_transitions'

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import NextLink from 'next/link'
 import { Container, Box, Heading, Image, useColorModeValue, Button } from '@chakra-ui/react'
 import Section from '../components/section';
 import Paragraph from '../components/paragraph';
+import Layout from '../components/layouts/article';
 import { ChevronRightIcon } from '@chakra-ui/icons';
 import { BioSection, BioYear } from '../components/bio';
 //import Link from 'next/link';
@@ -9,101 +10,101 @@ import { BioSection, BioYear } from '../components/bio';
 
 const Home = () => {
     return (
-        <Container >
-            <Box 
-            borderRadius="lg"
-            bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
-            p={3} 
-            mb={6} 
-            align='center'
-            >
-                Hello I&apos;m a full-stack developer based in Buenos Aires, Argentina! 
-            </Box>
-
-            <Box display={{md:'flex'}}>
-                <Box flexGrow={1}>
-                    <Heading as='h2' variant='page-title'>
-                        Nicolas Gonzalez
-                    </Heading>
-                    <p>Full Stack Developer</p>
-                </Box>
+        <Layout>
+            <Container >
                 <Box 
-                    flexShrink={0}
-                    mt={{base:4,md:0}}
-                    ml={{md:6}}
-                    align='center'
-
+                borderRadius="lg"
+                bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} 
+                p={3} 
+                mb={6} 
+                align='center'
                 >
-                    <Image 
-                        borderColor="whiteAlpha.800"
-                        borderWidth={2}
-                        borderStyle='solid'
-                        maxWidth='100px'
-                        display='inline-block'
-                        borderRadius="full"
-                        src='/images/nico_edit.jpg'
-                        alt='Profile Image'
-                    />
+                    Hello I&apos;m a full-stack developer based in Buenos Aires, Argentina! 
                 </Box>
-            </Box>
 
+                <Box display={{md:'flex'}}>
+                    <Box flexGrow={1}>
+                        <Heading as='h2' variant='page-title'>
+                            Nicolas Gonzalez
+                        </Heading>
+                        <p>Full Stack Developer</p>
+                    </Box>
+                    <Box 
+                        flexShrink={0}
+                        mt={{base:4,md:0}}
+                        ml={{md:6}}
+                        align='center'
 
-            <Section delay={0.1}>
-                <Heading as='h3' variant='section-title'>
-                    About me
-                </Heading>
-                <Paragraph>
-                    I love the world of technology, 
-                    I am passionate about solving programming problems and innovating code.
-
-                    I like other fields like cinema, exercising and sports. 
-                    I am currently looking for a job where I can put my skills and knowledge into practice to achieve great goals.
-                    I consider myself a lively, positive, cheerful and respectful person. These qualities have always helped me to work in a team in an excellent way.
-                    {/* <NextLink href='/works/inkdrop'>
-                        <Link>Inkdrop</Link>
-                    </NextLink> */}
-                </Paragraph>
-                <Box align='center' my={4}>
-                    <NextLink href='/works'>
-                        <Button
-                            rightIcon={<ChevronRightIcon />}
-                            colorScheme='teal'
-                        >
-                        Projects
-                        </Button>
-                    </NextLink>
+                    >
+                        <Image 
+                            borderColor="whiteAlpha.800"
+                            borderWidth={2}
+                            borderStyle='solid'
+                            maxWidth='100px'
+                            display='inline-block'
+                            borderRadius="full"
+                            src='/images/nico_edit.jpg'
+                            alt='Profile Image'
+                        />
+                    </Box>
                 </Box>
-            </Section>
 
-            <Section delay={0.2}>
-                <Heading as='h3' variant='section-title'>
-                    Bio
-                </Heading>
-                <BioSection>
-                    <BioYear>2019</BioYear>
-                    I start my studies in engineering at the FIUBA (Facultad de Ingenieria de la Universidad de Buenos Aires).
-                </BioSection>
-                <BioSection>
-                    <BioYear>2020</BioYear>
-                    The pandemic starts and I continue the university. I started to learn Python and love programming.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2020</BioYear>
-                    I started my studies as Python Developer at 
-                    <a target='_blank' href='https://www.educacionit.com/carrera-python' rel="noreferrer"> Educacion IT</a>.
-                </BioSection>
-                <BioSection>
-                    <BioYear>2021</BioYear>
-                    I did my first job as developer for a glass startup. 
-                </BioSection>
-                <BioSection>
-                    <BioYear>2022</BioYear>
-                    I start my studies at SoyHenry Bootcamp for Full-Stack Developer. 
-                    In July I was certified as Full-stack Developer by soyHenry.
-                </BioSection>
-                
-            </Section>
-        </Container>
+
+                <Section delay={0.1}>
+                    <Heading as='h3' variant='section-title'>
+                        About me
+                    </Heading>
+                    <Paragraph>
+                        I love the world of technology, 
+                        I am passionate about solving programming problems and innovating code.
+
+                        I like other fields like cinema, exercising and sports. 
+                        I am currently looking for a job where I can put my skills and knowledge into practice to achieve great goals.
+                        I consider myself a lively, positive, cheerful and respectful person. These qualities have always helped me to work in a team in an excellent way.
+                        
+                    </Paragraph>
+                    <Box align='center' my={4}>
+                        <NextLink href='/works'>
+                            <Button
+                                rightIcon={<ChevronRightIcon />}
+                                colorScheme='teal'
+                            >
+                            Projects
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Section>
+
+                <Section delay={0.2}>
+                    <Heading as='h3' variant='section-title'>
+                        Bio
+                    </Heading>
+                    <BioSection>
+                        <BioYear>2019</BioYear>
+                        I start my studies in engineering at the FIUBA (Facultad de Ingenieria de la Universidad de Buenos Aires).
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        The pandemic starts and I continue the university. I started to learn Python and love programming.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2020</BioYear>
+                        I started my studies as Python Developer at 
+                        <a target='_blank' href='https://www.educacionit.com/carrera-python' rel="noreferrer"> Educacion IT</a>.
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2021</BioYear>
+                        I did my first job as developer for a glass startup. 
+                    </BioSection>
+                    <BioSection>
+                        <BioYear>2022</BioYear>
+                        I start my studies at SoyHenry Bootcamp for Full-Stack Developer. 
+                        In July I was certified as Full-stack Developer by soyHenry.
+                    </BioSection>
+                    
+                </Section>
+            </Container>
+        </Layout>
     )
 }
 
---
+++ pages/works.js
@@ -0,0 +1,31 @@
+import { Container, Box, Heading, SimpleGrid, Divider } from "@chakra-ui/react";
+import Section from '../components/section'
+import { WorkGridItem } from "../components/grid-item";
+import Layout from '../components/layouts/article';
+//Aca vamos a importar las fotos
+import yo from '../public/images/nico_edit.jpg'
+
+const Works = () => {
+    return (
+        <Layout>
+            <Container>
+                <Heading as='h3' fontSize={20} mb={4}>
+                    Works
+                </Heading>
+                <SimpleGrid columns={[1,1,2]} gap={6}>
+                    <Section>
+                        <WorkGridItem 
+                            id='walknote' 
+                            title='API-Food'
+                            thumbnail={yo}
+                        >
+                        In this application you will be able to see different food recipes, with their information.  
+                        </WorkGridItem> 
+                    </Section>
+                </SimpleGrid>
+            </Container>
+        </Layout>
+    )
+}
+
+export default Works
