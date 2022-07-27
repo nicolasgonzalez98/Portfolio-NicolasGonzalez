7d26cfd2eeccc4d9027760918fa28ab104b37aa8
unknown <nicolasgonzalez470@gmail.com>
2022-07-27 17:24:19 -0300

    deploy

--- pages/works/api_foods.js
+++ pages/works/api_foods.js
@@ -7,7 +7,7 @@ import {
     useColorModeValue,
     UnorderedList,
     //SimpleGrid
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
 import Layout from '../../components/layouts/article'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Title, WorkImage, Meta } from '../../components/work'
