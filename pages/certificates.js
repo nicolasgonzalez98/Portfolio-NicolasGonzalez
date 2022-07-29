0f2f41e2ce67af10b7f91105a1645fa67453c446
unknown <nicolasgonzalez470@gmail.com>
2022-07-29 03:47:54 -0300

    fix certificates

--- pages/certificates.js
+++ pages/certificates.js
@@ -7,6 +7,7 @@ import Layout from "../components/layouts/article"
 //import img from '../public/images/certificados/desarrollo-web_page-0001.jpg'
 import ImageSlider from "../components/ImageSlider"
 import { SlideData } from "../components/resources/SlideData";
+import Section from "../components/section";
 
 
 const Certificate = () => {
@@ -14,10 +15,13 @@ const Certificate = () => {
     return (
         <Layout>
             <Container>
-                <Heading as='h3' fontSize={20} mb={4}>
-                    Certificates
-                </Heading>
-                <ImageSlider slides={SlideData} />
+                <Section delay={0.3}>
+                    <Heading as='h3' fontSize={20} mb={4}>
+                        Certificates
+                    </Heading>
+                    <ImageSlider slides={SlideData} />
+                </Section>
+                
             </Container>
         </Layout>
     )
