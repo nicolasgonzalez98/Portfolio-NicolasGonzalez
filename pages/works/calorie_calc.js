13a413e8895dd7bf5ef84278e3df8631bee016ce
nicolasgonzalez98 <nicolasgonzalez470@gmail.com>
2023-02-22 22:54:11 -0300

    foodCalc

--- pages/works/calorie_calc.js
+++ pages/works/calorie_calc.js
@@ -2,8 +2,16 @@ import Layout from "../../components/layouts/article"
 
 const Work = () => {
     return (
-        <Layout>
+        <Layout title={'Calorie calculator'}>
+            <Container>
+                <Title>Calorie calculator <Badge>2023</Badge></Title>
+            </Container>
 
+            <P style={{color: useColorModeValue('black', 'cyan')}}>
+                In this application you will be able to see the information about all the 
+                food items in the app. You can filter them by region and name.
+                You can toggle the color scheme between light and dark mode 
+            </P>
         </Layout>
     )
 }
