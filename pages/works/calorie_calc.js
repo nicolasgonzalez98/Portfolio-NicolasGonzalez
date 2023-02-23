4cd904b7df513dee7c3ac9c4e8f85d225cd07ebb
unknown <nicolasgonzalez470@gmail.com>
2023-02-23 00:52:22 -0300

    calorie_calc

--- pages/works/calorie_calc.js
+++ pages/works/calorie_calc.js
@@ -1,4 +1,18 @@
+import {
+    Container,
+    Badge,
+    Link,
+    List,
+    ListItem,
+    useColorModeValue,
+    UnorderedList,
+    //UnorderedList,
+    //SimpleGrid
+} from '@chakra-ui/react';
 import Layout from "../../components/layouts/article"
+import { ExternalLinkIcon } from '@chakra-ui/icons'
+import { Title, Meta, WorkImage } from "../../components/work";
+import P from '../../components/paragraph'
 
 const Work = () => {
     return (
@@ -7,11 +21,43 @@ const Work = () => {
                 <Title>Calorie calculator <Badge>2023</Badge></Title>
             </Container>
 
-            <P style={{color: useColorModeValue('black', 'cyan')}}>
-                In this application you will be able to see the information about all the 
-                food items in the app. You can filter them by region and name.
-                You can toggle the color scheme between light and dark mode 
+            <P style={{color: useColorModeValue('black', 'cyan')}} mb={5}>
+            ”Health is wealth” Obviously, you have heard it a thousand times but as we grow older we often realize it’s true and this app provides an interesting way to get started with the very boring term “dieting” as we already know “FITNESS START WITH WHAT WE EAT”.
+
+Let’s develop an interesting project – Calorie Calculator to record and estimate number of calories we need to consume daily. This app can also provide guidelines for gaining or losing weight.
             </P>
+
+            <List ml={4} my={4}>
+                <ListItem>
+                    <Meta>Website</Meta>
+                    <Link href="#" 
+                    //target='_blank'
+                    >
+                    No website<ExternalLinkIcon mx="2px" />
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Meta>Stack</Meta>
+                    Python, Django, django-filter
+                </ListItem>
+                <ListItem>
+                    <Meta>Source</Meta>
+                    <Link href='https://github.com/nicolasgonzalez98/CalorieCalc-dj-py-' target='_blank'>
+                        GitHub <ExternalLinkIcon mx="2px" />
+                    </Link>
+                </ListItem>
+                
+            </List>
+            <UnorderedList mb={5}>
+                <ListItem>Create your own food items.</ListItem>
+                <ListItem>Add food items to users!</ListItem>
+                <ListItem>Record and estimate number of calories we need to consume daily</ListItem>
+            </UnorderedList>
+            
+            <WorkImage src={'/images/CalorieCalc/home.jpg'} />
+            <WorkImage src={'/images/CalorieCalc/signIn.jpg'} />
+            <WorkImage src={'/images/CalorieCalc/addFoodItem.jpg'} />
+            <WorkImage src={'/images/CalorieCalc/foodItems.jpg'} />
         </Layout>
     )
 }
